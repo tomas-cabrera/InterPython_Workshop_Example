c6d35d8f7d01bc75c047e93c54373b316c8c15d8
tomas-cabrera <tcabrera@andrew.cmu.edu>
2024-11-08 17:00:45 -0500

    Add initial test cases for mean_mag() and min_mag()

--- lcanalyzer/models.py
+++ lcanalyzer/models.py
@@ -11,26 +11,27 @@ import pandas as pd
 import numpy as np
 from astropy.timeseries import LombScargle
 
+
 def load_dataset(filename):
     """Load a table from CSV file.
-    
+
     :param filename: The name of the .csv file to load
     :returns: pd.DataFrame with the data from the file.
     """
     return pd.read_csv(filename)
 
 
-def mean_mag(data,mag_col):
+def mean_mag(data, mag_col):
     """Calculate the mean magnitude of a lightcurve"""
     return data[mag_col].mean()
 
 
-def max_mag(data,mag_col):
+def max_mag(data, mag_col):
     """Calculate the max magnitude of a lightcurve"""
     return data[mag_col].max()
 
 
-def min_mag(data,mag_col):
+def min_mag(data, mag_col):
     """Calculate the min magnitude of a lightcurve
     :param data: pd.DataFrame with observed magnitudes for a single source.
     :param mag_col: a string with the name of the column for calculating the min value.
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,26 +2,80 @@
 
 import pandas as pd
 
+
 def test_max_mag_integers():
     # Test that max_mag function works for integers
     from lcanalyzer.models import max_mag
 
-    test_input_df = pd.DataFrame(data=[[1, 5, 3], 
-                                       [7, 8, 9], 
-                                       [3, 4, 1]], columns=list("abc"))
+    test_input_df = pd.DataFrame(
+        data=[[1, 5, 3], [7, 8, 9], [3, 4, 1]], columns=list("abc")
+    )
     test_input_colname = "a"
     test_output = 7
 
     assert max_mag(test_input_df, test_input_colname) == test_output
 
+
 def test_max_mag_zeros():
     # Test that max_mag function works for zeros
     from lcanalyzer.models import max_mag
 
-    test_input_df = pd.DataFrame(data=[[0, 0, 0], 
-                                       [0, 0, 0], 
-                                       [0, 0, 0]], columns=list("abc"))
+    test_input_df = pd.DataFrame(
+        data=[[0, 0, 0], [0, 0, 0], [0, 0, 0]], columns=list("abc")
+    )
     test_input_colname = "b"
     test_output = 0
 
     assert max_mag(test_input_df, test_input_colname) == test_output
+
+
+def test_min_mag_integers():
+    # Test that min_mag function works for integers
+    from lcanalyzer.models import min_mag
+
+    test_input_df = pd.DataFrame(
+        data=[[1, 5, 3], [7, 8, 9], [3, 4, 1]], columns=list("abc")
+    )
+    test_input_colname = "a"
+    test_output = 1
+
+    assert min_mag(test_input_df, test_input_colname) == test_output
+
+
+def test_min_mag_zeros():
+    # Test that min_mag function works for zeros
+    from lcanalyzer.models import min_mag
+
+    test_input_df = pd.DataFrame(
+        data=[[0, 0, 0], [0, 0, 0], [0, 0, 0]], columns=list("abc")
+    )
+    test_input_colname = "b"
+    test_output = 0
+
+    assert min_mag(test_input_df, test_input_colname) == test_output
+
+
+def test_mean_mag_integers():
+    # Test that mean_mag function works for integers
+    from lcanalyzer.models import mean_mag
+
+    test_input_df = pd.DataFrame(
+        data=[[1, 5, 3], [7, 8, 9], [3, 4, 1]], columns=list("abc")
+    )
+    test_input_colname = "a"
+    test_output = 11 / 3
+
+    assert mean_mag(test_input_df, test_input_colname) == test_output
+
+
+def test_mean_mag_zeros():
+    # Test that mean_mag function works for zeros
+    from lcanalyzer.models import mean_mag
+
+    test_input_df = pd.DataFrame(
+        data=[[0, 0, 0], [0, 0, 0], [0, 0, 0]], columns=list("abc")
+    )
+    test_input_colname = "b"
+    test_output = 0
+
+    assert mean_mag(test_input_df, test_input_colname) == test_output
